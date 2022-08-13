89fee30ccded5a5560eaf5fe594db1c24d02bd6b
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-13 15:08:12 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,22 @@
 class Settings:
     def __init__(self):
+        # Настройки экрана
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (16, 20, 88)
-        self.ship_speed = 1.5
+
+        # Настройки корабля
+        self.ship_speed = 5
+
+        # Настройки пули
+        self.bullet_speed = 5
+        self.bullet_width = 3
+        self.bullet_height = 30
+        self.bullet_color = (255, 50, 0)
+        self.bullets_allowed = 5
+
+        # Настройки пришельцев
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
