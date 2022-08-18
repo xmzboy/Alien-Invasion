c085e0431c8ee16f7fa665db4e2ffed11293e4fd
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-18 15:47:50 +0300

    Update game_stats.py
    Add new attributes

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,11 @@ class GameStats:
         # Старт игры в неактивном состоянии
         self.game_active = False
 
+        # Наилучший результат
+        self.high_score = 0
+
     def reset_stats(self):
         """Сброс статистики"""
         self.ship_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
