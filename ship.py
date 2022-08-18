e4b53e7d8c77444236f54f2121ff800c3bc2354b
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-18 15:48:32 +0300

    Update ship.py
    Add parent class

--- ship.py
+++ ship.py
@@ -1,11 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
-
-class Ship:
+class Ship(Sprite):
     """Класс корабля"""
 
     def __init__(self, ai_game):
         """Инициализация корабля и установка начального положения"""
+        super().__init__()
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
