32f03637073fa6afcaff46a109f994adbb51b863
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-19 20:14:22 +0300

    Update button.py
    Add hard mode button

--- button.py
+++ button.py
@@ -3,25 +3,31 @@ import pygame.font
 
 class Button:
     """Класс кнопки"""
-    def __init__(self, ai_game, msg):
+    def __init__(self, ai_game, msg, color, shift=0):
         """Инициализация кнопки"""
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
         self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
+        self.button_color = color
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
         self.rect = pygame.Rect(0, 0, self.width, self.height)
-        self.rect.center = self.screen_rect.center
+        if not shift:
+            self.rect.center = self.screen_rect.center
+        else:
+            self.rect.center = (960, 640)
 
-        self._prep_msg(msg)
+        self._prep_msg(msg, shift)
 
-    def _prep_msg(self, msg):
+    def _prep_msg(self, msg, shift):
         self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect.center = self.rect.center
+        if not shift:
+            self.msg_image_rect.center = self.rect.center
+        else:
+            self.msg_image_rect.center = (960, 640)
 
     def draw_button(self):
         self.screen.fill(self.button_color, self.rect)
