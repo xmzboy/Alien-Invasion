f680daa1e5ca6c40a0b062567ebea586e60cd173
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-21 10:56:05 +0300

    Update game_stats.py
    rename variable

--- game_stats.py
+++ game_stats.py
@@ -14,6 +14,6 @@ class GameStats:
 
     def reset_stats(self):
         """Сброс статистики"""
-        self.ship_left = self.settings.ship_limit
+        self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
