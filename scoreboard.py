ded89a2523422a11dd2317eea9758640b3627a17
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-21 10:55:40 +0300

    Update scoreboard.py
    Add level, record and ship health

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,8 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
+
 
 class Scoreboard:
 
@@ -7,19 +11,56 @@ class Scoreboard:
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
-
+        self.ai_game = ai_game
         self.text_color = (255, 255, 255)
-        self.font = pygame.font.SysFont(None, 72)
+        self.font = pygame.font.SysFont(None, 64)
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
-        score_str = str(self.stats.score)
-        self.score_image = self.font.render(score_str, True, self.text_color, (0, 0, 0))
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
+        self.score_image = self.font.render(f'Score: {score_str}', True, self.text_color, (0, 0, 0))
         self.score_image.set_alpha(127)
         #self.score_image = pygame.font.Font(None, 100).render(score_str, True, self.text_color, (0,100,100,0))
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(f'Record: {high_score_str}', True, self.text_color, (0, 0, 0))
+        self.high_score_image.set_alpha(127)
+        self.high_score_image_rect = self.high_score_image.get_rect()
+        self.high_score_image_rect.centerx = self.screen_rect.centerx
+        self.high_score_image_rect.top = self.high_score_image_rect.top
+
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_image_rect)
+        self.screen.blit(self.level_image, self.level_image_rect)
+        self.ships.draw(self.screen)
+
+    def check_high_score(self):
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
+    def prep_level(self):
+        level_str = str(f'Level: {self.stats.level}')
+        self.level_image = self.font.render(level_str, True, self.text_color, (0, 0, 0))
+        self.level_image.set_alpha(127)
+        self.level_image_rect = self.level_image.get_rect()
+        self.level_image_rect.right = self.score_rect.right
+        self.level_image_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
